79338b3f2c240f24efa26c5a6b6f88187a833756
Gereon Kremer <gereon.kremer@gmail.com>
2025-04-17 10:26:05 +0200

    remove old stuff, add some mocks

--- certora/mocks/IERC1822ProxiableMock.sol
+++
@@ -1,9 +0,0 @@
-import { IERC1822Proxiable } from "@openzeppelin-contracts/interfaces/draft-IERC1822.sol";
-
-contract IERC1822ProxiableMock is IERC1822Proxiable {
-    bytes32 public constant PROXIABLE_UUID = keccak256("some-random-string");
-
-    function proxiableUUID() external view override returns (bytes32) {
-        return PROXIABLE_UUID;
-    }
-}
