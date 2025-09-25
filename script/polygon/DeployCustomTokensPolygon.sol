15d58c4b57cc9a80a51e5aaf9f5d59dd8b8c8abc
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-09-25 19:06:16 +0200

    script(polygon): update

--- script/polygon/DeployCustomTokensPolygon.sol
+++ script/polygon/DeployCustomTokensPolygon.sol
@@ -15,6 +15,9 @@ import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transpa
 /// @notice Creates a singleton `GenericCustomTokenPolygon` implementation and a `TransparentUpgradeableProxy` for each Custom Token, points the proxies to the implementation, and initializes them.
 /// @dev The Custom Tokens need to be custom mapped on PoS Portal subsequently. Please refer to `src/secondary-chain/polygon/README.md` for more information.
 contract DeployCustomTokensPolygon is Script {
+    // Secondary Chain name.
+    string public secondaryChainName;
+
     // Deployer address.
     address public deployerAddress;
 
@@ -22,10 +25,8 @@ contract DeployCustomTokensPolygon is Script {
     address public genericCustomTokenPolygonImplementation;
 
     // `GenericCustomTokenPolygon` proxies.
-    GenericCustomTokenPolygon public vbEth;
     GenericCustomTokenPolygon public vbUsdc;
     GenericCustomTokenPolygon public vbUsdt;
-    GenericCustomTokenPolygon public vbWbtc;
     GenericCustomTokenPolygon public vbUsds;
 
     // Bridge address.
@@ -38,11 +39,13 @@ contract DeployCustomTokensPolygon is Script {
     /// @dev You can customize the setup here.
     function setUp() public {
         // Set the inputs.
+        secondaryChainName = "";
         deployerAddress = 0x0000000000000000000000000000000000000000;
         childChainManagerAddress = 0x0000000000000000000000000000000000000000;
         ownerAddress = 0x0000000000000000000000000000000000000000;
 
         // Check the inputs.
+        require(bytes(secondaryChainName).length != 0, "Aborted: `secondaryChainName` not set");
         require(deployerAddress != address(0), "Aborted: `deployerAddress` not set");
         require(childChainManagerAddress != address(0), "Aborted: `childChainManagerAddress` not set");
         require(ownerAddress != address(0), "Aborted: `ownerAddress` not set");
@@ -54,59 +57,55 @@ contract DeployCustomTokensPolygon is Script {
         console.log("Running `DeployCustomTokensPolygon` script...");
 
         // Switch to Polygon.
-        _createSelectFork("polygon");
+        _createSelectFork(secondaryChainName);
 
         // Create a singleton `GenericCustomTokenPolygon` implementation.
         genericCustomTokenPolygonImplementation = _createGenericCustomTokenPolygonImplementation();
 
-        // Create and proxify `GenericCustomTokenPolygon` for each Custom Token.
-        vbEth = _createAndProxifyGenericCustomTokenPolygon(genericCustomTokenPolygonImplementation);
-        vbUsdc = _createAndProxifyGenericCustomTokenPolygon(genericCustomTokenPolygonImplementation);
-        vbUsdt = _createAndProxifyGenericCustomTokenPolygon(genericCustomTokenPolygonImplementation);
-        vbWbtc = _createAndProxifyGenericCustomTokenPolygon(genericCustomTokenPolygonImplementation);
-        vbUsds = _createAndProxifyGenericCustomTokenPolygon(genericCustomTokenPolygonImplementation);
+        // Proxify and initialize vbUSDC.
+        {
+            bytes[] memory reinitializeData = new bytes[](1);
 
-        // Initialize each `GenericCustomTokenPolygon` proxy.
-        _reinitialize1({
-            genericCustomTokenPolygon: vbEth,
-            owner_: ownerAddress,
-            name_: "Vault Bridge ETH",
-            symbol_: "vbETH",
-            originalUnderlyingTokenDecimals_: 18,
-            childChainManager_: childChainManagerAddress
-        });
-        _reinitialize1({
-            genericCustomTokenPolygon: vbUsdc,
-            owner_: ownerAddress,
-            name_: "Vault Bridge USDC",
-            symbol_: "vbUSDC",
-            originalUnderlyingTokenDecimals_: 6,
-            childChainManager_: childChainManagerAddress
-        });
-        _reinitialize1({
-            genericCustomTokenPolygon: vbUsdt,
-            owner_: ownerAddress,
-            name_: "Vault Bridge USDT",
-            symbol_: "vbUSDT",
-            originalUnderlyingTokenDecimals_: 6,
-            childChainManager_: childChainManagerAddress
-        });
-        _reinitialize1({
-            genericCustomTokenPolygon: vbWbtc,
-            owner_: ownerAddress,
-            name_: "Vault Bridge WBTC",
-            symbol_: "vbWBTC",
-            originalUnderlyingTokenDecimals_: 8,
-            childChainManager_: childChainManagerAddress
-        });
-        _reinitialize1({
-            genericCustomTokenPolygon: vbUsds,
-            owner_: ownerAddress,
-            name_: "Vault Bridge USDS",
-            symbol_: "vbUSDS",
-            originalUnderlyingTokenDecimals_: 18,
-            childChainManager_: childChainManagerAddress
-        });
+            reinitializeData[0] = abi.encodeCall(
+                GenericCustomTokenPolygon.reinitialize1,
+                (ownerAddress, "Vault Bridge USDC", "vbUSDC", 6, childChainManagerAddress)
+            );
+
+            vbUsdc = _proxifyAndInitializeGenericCustomTokenPolygon(
+                genericCustomTokenPolygonImplementation,
+                abi.encodeCall(GenericCustomTokenPolygon.reinitialize, (reinitializeData))
+            );
+        }
+
+        // Proxify and initialize vbUSDT.
+        {
+            bytes[] memory reinitializeData = new bytes[](1);
+
+            reinitializeData[0] = abi.encodeCall(
+                GenericCustomTokenPolygon.reinitialize1,
+                (ownerAddress, "Vault Bridge USDT", "vbUSDT", 6, childChainManagerAddress)
+            );
+
+            vbUsdt = _proxifyAndInitializeGenericCustomTokenPolygon(
+                genericCustomTokenPolygonImplementation,
+                abi.encodeCall(GenericCustomTokenPolygon.reinitialize, (reinitializeData))
+            );
+        }
+
+        // Proxify and initialize vbUSDS.
+        {
+            bytes[] memory reinitializeData = new bytes[](1);
+
+            reinitializeData[0] = abi.encodeCall(
+                GenericCustomTokenPolygon.reinitialize1,
+                (ownerAddress, "Vault Bridge USDS", "vbUSDS", 18, childChainManagerAddress)
+            );
+
+            vbUsds = _proxifyAndInitializeGenericCustomTokenPolygon(
+                genericCustomTokenPolygonImplementation,
+                abi.encodeCall(GenericCustomTokenPolygon.reinitialize, (reinitializeData))
+            );
+        }
 
         console.log("Finished running `DeployCustomTokensPolygon` script");
     }
@@ -128,12 +127,12 @@ contract DeployCustomTokensPolygon is Script {
         return address(implementation);
     }
 
-    /// @notice Creates a `TransparentUpgradeableProxy` for a Custom Token and points it to the `GenericCustomTokenPolygon` implementation.
-    function _createAndProxifyGenericCustomTokenPolygon(address genericCustomTokenPolygonImplementation_)
-        internal
-        returns (GenericCustomTokenPolygon)
-    {
-        console.log("Creating and proxifying `GenericCustomTokenPolygon`...");
+    /// @notice Creates a `TransparentUpgradeableProxy` for a Custom Token, points it to the `GenericCustomTokenPolygon` implementation, and initializes it.
+    function _proxifyAndInitializeGenericCustomTokenPolygon(
+        address genericCustomTokenPolygonImplementation_,
+        bytes memory initializationData_
+    ) internal returns (GenericCustomTokenPolygon) {
+        console.log("Proxifying and initializing `GenericCustomTokenPolygon`...");
 
         // Check the input.
         require(
@@ -144,45 +143,21 @@ contract DeployCustomTokensPolygon is Script {
         _startBroadcast();
 
         // Create a `TransparentUpgradeableProxy` and point it to the `GenericCustomTokenPolygon` implementation.
-        TransparentUpgradeableProxy proxy =
-            new TransparentUpgradeableProxy(genericCustomTokenPolygonImplementation_, deployerAddress, "");
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            genericCustomTokenPolygonImplementation_, deployerAddress, initializationData_
+        );
 
         _stopBroadcast();
 
-        console.log("`GenericCustomTokenPolygon` created and proxified:", address(proxy));
+        console.log(GenericCustomTokenPolygon(address(proxy)).name(), "proxified and initialized:", address(proxy));
 
         // Return a `GenericCustomTokenPolygon`.
         return GenericCustomTokenPolygon(address(proxy));
     }
 
-    function _reinitialize1(
-        GenericCustomTokenPolygon genericCustomTokenPolygon,
-        address owner_,
-        string memory name_,
-        string memory symbol_,
-        uint8 originalUnderlyingTokenDecimals_,
-        address childChainManager_
-    ) internal {
-        console.log("Executing `reinitialize1` for", name_, "...");
-
-        // Check the input.
-        require(address(genericCustomTokenPolygon) != address(0), "Aborted: `genericCustomTokenPolygon` not set");
-
-        _startBroadcast();
-
-        // Execute `reinitialize1`.
-        genericCustomTokenPolygon.reinitialize1(
-            owner_, name_, symbol_, originalUnderlyingTokenDecimals_, childChainManager_
-        );
-
-        _stopBroadcast();
-
-        console.log("Successfully executed `reintialize1` for", name_);
-    }
-
-    function _createSelectFork(string memory chainName) internal {
-        vm.createSelectFork(vm.rpcUrl(chainName));
-        console.log("Switched to", chainName, "chain");
+    function _createSelectFork(string memory chainName_) internal {
+        vm.createSelectFork(vm.rpcUrl(chainName_));
+        console.log("Switched to", chainName_, "chain");
     }
 
     function _startBroadcast() internal {
