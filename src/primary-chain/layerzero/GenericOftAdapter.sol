f83028387d1c64d8b017f12a8578a8089177d286
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-10-14 13:04:52 +0200

    fix: generic oft adapter

--- src/primary-chain/layerzero/GenericOftAdapter.sol
+++ src/primary-chain/layerzero/GenericOftAdapter.sol
@@ -8,12 +8,49 @@ pragma solidity 0.8.29;
 // Main functionality.
 import {OFTAdapterUpgradeable} from "@layerzerolabs-oft-evm-upgradeable/contracts/oft/OFTAdapterUpgradeable.sol";
 
+// Other functionality.
+import {ReentrancyGuardTransientUpgradeable} from
+    "@openzeppelin-contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
+import {InitializationCounterUpgradeable} from "../../etc/InitializationCounterUpgradeable.sol";
+
 /// @title Generic OFT Adapter
 /// @author See https://github.com/agglayer/vault-bridge
-contract GenericOftAdapter is OFTAdapterUpgradeable {
+contract GenericOftAdapter is
+    OFTAdapterUpgradeable,
+    ReentrancyGuardTransientUpgradeable,
+    InitializationCounterUpgradeable
+{
     // -----================= ::: SETUP ::: =================-----
 
     constructor(address _token, address _lzEndpoint) OFTAdapterUpgradeable(_token, _lzEndpoint) {
         _disableInitializers();
+    }
+
+    function reinitialize1(address _owner, address _delegate)
+        external
+        reinitializer(_incrementGlobalInitializationCounter(1))
+        nonReentrant
+    {
+        __Ownable_init(_owner);
+        __OFTAdapter_init(_delegate);
+        __ReentrancyGuardTransient_init();
+    }
+
+    /*
+    /// @dev How to add a new reinitializer:
+    function reinitialize2()
+        external
+        reinitializer(_incrementGlobalInitializationCounter(2))
+        nonReentrant
+    {}
+    */
+
+    // @remind Document (the entire function).
+    function reinitialize(bytes[] calldata reinitializeData) external {
+        bytes4[] memory reinitializeSelectors = new bytes4[](1);
+
+        reinitializeSelectors[0] = this.reinitialize1.selector;
+
+        _reinitialize(reinitializeSelectors, reinitializeData);
     }
 }
