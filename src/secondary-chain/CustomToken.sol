5bbbcf3ff4564a22db24e285c32b21318b127240
Ignasi <ignasi@polygon.technology>
2025-11-17 13:58:56 +0100

    Fix conflicts customToken

--- src/secondary-chain/CustomToken.sol
+++ src/secondary-chain/CustomToken.sol
@@ -1,37 +1,43 @@
 // SPDX-License-Identifier: LicenseRef-PolygonLabs-Source-Available
+// Vault Bridge (last updated v1.0.0) (secondary-chain/CustomToken.sol)
+
 pragma solidity 0.8.29;
 
 // Main functionality.
-import {ERC20PermitUpgradeable} from
-    "@openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {
+    ERC20PermitUpgradeable
+} from "@openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
 // Other functionality.
 import {Initializable} from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin-contracts-upgradeable/utils/PausableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin-contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {InitializationCounterUpgradeable} from "../etc/InitializationCounterUpgradeable.sol";
 import {Versioned} from "../etc/Versioned.sol";
 
 /// @title Custom Token
 /// @author See https://github.com/agglayer/vault-bridge
-/// @notice A Custom Token is an ERC-20 token deployed on Layer Ys to represent the native version of the original underlying token from Layer X on Layer Y.
+/// @notice A Custom Token is an optional ERC-20 token on Secondary Chains to represent the 'native' version of the original underlying token from Primary Chain on Secondary Chain, ideally (or, simply, the upgraded version of the bridged vbToken).
 /// @dev A base contract used to create Custom Tokens.
-/// @dev @note IMPORTANT: Custom Token MUST be custom mapped to the corresponding vbToken on LxLy Bridge on Layer Y and MUST give the minting and burning permission to LxLy Bridge and Native Converter. It MAY have a transfer fee.
+/// @dev @note IMPORTANT: Custom Token MUST be used as the new implementation for the bridged vbToken or be custom mapped to the corresponding vbToken on Agglayer Bridge on Secondary Chain, and MUST give the minting and burning permission to Agglayer Bridge and Native Converter. It MAY have a transfer fee.
 abstract contract CustomToken is
     Initializable,
     AccessControlUpgradeable,
     PausableUpgradeable,
     ReentrancyGuardUpgradeable,
     ERC20PermitUpgradeable,
-    IVersioned
+    InitializationCounterUpgradeable,
+    Versioned
 {
     /// @dev Storage of Custom Token contract.
     /// @dev It's implemented on a custom ERC-7201 namespace to reduce the risk of storage collisions when using with upgradeable contracts.
     /// @custom:storage-location erc7201:agglayer.vault-bridge.CustomToken.storage
     struct CustomTokenStorage {
         uint8 decimals;
-        address lxlyBridge;
+        address bridge;
         address nativeConverter;
+        uint256 _secondaryChainBalance;
     }
 
     /// @dev The storage slot at which Custom Token storage starts, following the EIP-7201 standard.
@@ -40,6 +46,7 @@ abstract contract CustomToken is
         hex"0300d81ec8b5c42d6bd2cedd81ce26f1003c52753656b7512a8eef168b702500";
 
     // Basic roles.
+    // @remind Document.
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     // Errors.
@@ -48,32 +55,22 @@ abstract contract CustomToken is
     error InvalidName();
     error InvalidSymbol();
     error InvalidOriginalUnderlyingTokenDecimals();
-    error InvalidLxLyBridge();
-    error InvalidNativeConverter();
-
-    // -----================= ::: MODIFIERS ::: =================-----
-
-    /// @dev Checks if the sender is LxLy Bridge or Native Converter.
-    /// @dev This modifier is used to restrict the minting and burning of Custom Token.
-    modifier onlyLxlyBridgeAndNativeConverter() {
-        CustomTokenStorage storage $ = _getCustomTokenStorage();
-
-        // Only LxLy Bridge and Native Converter can mint and burn Custom Token.
-        require(msg.sender == $.lxlyBridge || msg.sender == $.nativeConverter, Unauthorized());
-
-        _;
-    }
+    error InvalidBridge();
+    error BridgeAlreadySet();
+    error NativeConverterAlreadySet();
+    error FunctionNotSupportedWithThisBridgeProvider();
 
     // -----================= ::: SETUP ::: =================-----
 
-    /// @param originalUnderlyingTokenDecimals_ The number of decimals of the original underlying token on Layer X. Custom Token will have the same number of decimals as the original underlying token.
+    /// @dev Preserves the `name` and `symbol` of the bridged vbToken.
+    /// @param originalUnderlyingTokenDecimals_ The number of decimals of the original underlying token on Primary Chain. Custom Token will have the same number of decimals as the original underlying token.
     /// @param nativeConverter_ The address of Native Converter for this Custom Token.
-    function __CustomToken_init(
+    function __CustomToken_init1(
         address owner_,
-        string calldata name_,
-        string calldata symbol_,
+        string memory name_,
+        string memory symbol_,
         uint8 originalUnderlyingTokenDecimals_,
-        address lxlyBridge_,
+        address bridge_,
         address nativeConverter_
     ) internal onlyInitializing {
         CustomTokenStorage storage $ = _getCustomTokenStorage();
@@ -83,8 +80,7 @@ abstract contract CustomToken is
         require(bytes(name_).length > 0, InvalidName());
         require(bytes(symbol_).length > 0, InvalidSymbol());
         require(originalUnderlyingTokenDecimals_ > 0, InvalidOriginalUnderlyingTokenDecimals());
-        require(lxlyBridge_ != address(0), InvalidLxLyBridge());
-        require(nativeConverter_ != address(0), InvalidNativeConverter());
+        require(bridge_ != address(0), InvalidBridge());
 
         // Initialize the inherited contracts.
         __ERC20_init(name_, symbol_);
@@ -102,26 +98,45 @@ abstract contract CustomToken is
 
         // Initialize the storage.
         $.decimals = originalUnderlyingTokenDecimals_;
-        $.lxlyBridge = lxlyBridge_;
+        $.bridge = bridge_;
         $.nativeConverter = nativeConverter_;
     }
+
+    // @remind Document (the entire function).
+    function __CustomToken_init2()
+        internal
+        onlyInitializing
+        incrementsLocalInitializationCounter(1)
+        incrementsLocalInitializationCounter(2)
+    {
+        // Empty function body.
+    }
+
+    /*
+    /// @dev How to add a new init step:
+    function __CustomToken_init3() internal onlyInitializing incrementsLocalInitializationCounter(3) {}
+    */
+
+    // @remind Document.
+    function _CUSTOM_TOKEN_INIT_2_COMPATIBLE() internal pure virtual;
 
     // -----================= ::: STORAGE ::: =================-----
 
     /// @notice The number of decimals of Custom Token.
-    /// @notice The number of decimals is the same as that of the original underlying token on Layer X.
+    /// @notice The number of decimals is the same as that of the original underlying token on Primary Chain.
     function decimals() public view override returns (uint8) {
         CustomTokenStorage storage $ = _getCustomTokenStorage();
         return $.decimals;
     }
 
-    /// @notice LxLy Bridge, which connects AggLayer networks.
-    function lxlyBridge() public view returns (address) {
+    /// @notice The contract that connects Custom Token to Primary Chain.
+    function bridge() public view returns (address) {
         CustomTokenStorage storage $ = _getCustomTokenStorage();
-        return $.lxlyBridge;
+        return $.bridge;
     }
 
     /// @notice The address of Native Converter for this Custom Token.
+    /// @return Returns `address(0)` if Native Converter is not connected.
     function nativeConverter() public view returns (address) {
         CustomTokenStorage storage $ = _getCustomTokenStorage();
         return $.nativeConverter;
@@ -137,30 +152,23 @@ abstract contract CustomToken is
     // -----================= ::: ERC-20 ::: =================-----
 
     /// @dev Pausable ERC-20 `transfer` function.
-    function transfer(address to, uint256 value) public virtual override whenNotPaused returns (bool) {
+    function transfer(address to, uint256 value) public override whenNotPaused returns (bool) {
         return super.transfer(to, value);
     }
 
     /// @dev Pausable ERC-20 `transferFrom` function.
-    function transferFrom(address from, address to, uint256 value)
-        public
-        virtual
-        override
-        whenNotPaused
-        returns (bool)
-    {
+    function transferFrom(address from, address to, uint256 value) public override whenNotPaused returns (bool) {
         return super.transferFrom(from, to, value);
     }
 
     /// @dev Pausable ERC-20 `approve` function.
-    function approve(address spender, uint256 value) public virtual override whenNotPaused returns (bool) {
+    function approve(address spender, uint256 value) public override whenNotPaused returns (bool) {
         return super.approve(spender, value);
     }
 
     /// @dev Pausable ERC-20 Permit `permit` function.
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
         public
-        virtual
         override
         whenNotPaused
     {
@@ -169,35 +177,44 @@ abstract contract CustomToken is
 
     // -----================= ::: CUSTOM TOKEN ::: =================-----
 
-    /// @notice Mints Custom Tokens to the recipient.
-    /// @notice This function can be called by LxLy Bridge and Native Converter only.
-    function mint(address account, uint256 value)
-        external
-        whenNotPaused
-        onlyLxlyBridgeAndNativeConverter
-        nonReentrant
-    {
-        // @remind Document.
-        if (account == address(0)) return;
-
-        _mint(account, value);
+    modifier bridgeInController(uint256 value) {
+        CustomTokenStorage storage $ = _getCustomTokenStorage();
+        _;
+        if (msg.sender == $.bridge) $._secondaryChainBalance += value;
     }
 
-    /// @notice Burns Custom Tokens from a holder.
-    /// @notice This function can be called by LxLy Bridge and Native Converter only.
-    function burn(address account, uint256 value)
-        external
-        whenNotPaused
-        onlyLxlyBridgeAndNativeConverter
-        nonReentrant
-    {
-        _burn(account, value);
+    modifier bridgeOutController(uint256 value) {
+        CustomTokenStorage storage $ = _getCustomTokenStorage();
+        if (msg.sender == $.bridge) $._secondaryChainBalance -= value;
+        _;
+    }
+
+    // @remind Document.
+    function _CUSTOM_TOKEN_IS_MINTABLE_BURNABLE() internal virtual;
+
+    // @remind Document (the entire function).
+    function setBridge(address bridge_) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        CustomTokenStorage storage $ = _getCustomTokenStorage();
+
+        require($.bridge.codehash == keccak256(hex"60006000fd"), BridgeAlreadySet());
+        require(bridge_ != address(0), InvalidBridge());
+
+        $.bridge = bridge_;
+    }
+
+    // @remind Document (the entire function).
+    function setNativeConverter(address nativeConverter_) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        CustomTokenStorage storage $ = _getCustomTokenStorage();
+
+        require($.nativeConverter == address(0), NativeConverterAlreadySet());
+
+        $.nativeConverter = nativeConverter_;
     }
 
     // -----================= ::: ADMIN ::: =================-----
 
     /// @notice Prevents usage of functions with the `whenNotPaused` modifier.
-    /// @notice This function can be called by the pauser only.
+    /// @notice This function can be called by a pauser only.
     function pause() external onlyRole(PAUSER_ROLE) nonReentrant {
         _pause();
     }
