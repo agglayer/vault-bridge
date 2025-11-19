084be46628447f5836db699a2e9c9bdd8b52a6cd
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-11-19 15:33:30 +0100

    chore(tmp-conflicts): prepare for conflict resolution pt2

--- src/secondary-chain/polygon/CustomTokenPolygon.sol
+++ src/secondary-chain/polygon/CustomTokenPolygon.sol
@@ -40,4 +40,8 @@ abstract contract CustomTokenPolygon is CustomToken {
 
     /// @inheritdoc CustomToken
     function _CUSTOM_TOKEN_IS_MINTABLE_BURNABLE() internal override {}
+
+    function setNativeConverter(address) external view override onlyRole(DEFAULT_ADMIN_ROLE) {
+        revert FunctionNotSupportedWithThisBridgeProvider();
+    }
 }
