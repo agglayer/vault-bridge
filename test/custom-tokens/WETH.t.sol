ea53d48e8ecefdb2ce46b6587819c305cdb6735b
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-12-15 17:06:49 +0100

    test: weth version

--- test/custom-tokens/WETH.t.sol
+++ test/custom-tokens/WETH.t.sol
@@ -72,7 +72,7 @@ contract WETHTest is Test {
     }
 
     function test_wETH_version() public view {
-        assertEq(wETH.version(), "0.5.1");
+        assertEq(wETH.version(), "0.5.0");
     }
 
     function test_onlyIfGasTokenIsEth() public {
